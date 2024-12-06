cbe12160cf0951013eda7b188433b587928149fd
WX <wxu81@jh.edu>
2024-12-06 00:18:46 -0500

    Update parameters

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import infer
 import RecsUI
 
 model = factorize.PMF("data/ratings_train.csv")
-U, V, cov_U, cov_V, _ = model.fit(100, num_epochs=1000)
+U, V, cov_U, cov_V, _ = model.fit(25, num_epochs=1200)
 predictor = infer.Predictor(U, V, cov_U, cov_V)
 interface = RecsUI.RecsUI("data/metadata.csv", predictor)
 interface.main_loop()
--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import infer
-from scipy.stats import norm
 
 class Tester:
     def __init__(self, ratings_test_path: str):
